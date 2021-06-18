8a87a4a7f119f931004a9c369b7cb7c098c47e87
Fabiano Massignani Figueredo <40632931+fabiano1606@users.noreply.github.com>
2021-06-18 01:11:48 -0300

    controllers inicias

--- src/routes/produto-route.js
+++ src/routes/produto-route.js
@@ -2,25 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
+const controlle = require('../controllers/produto-contoller');
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
+router.post('/', controlle.post);
 
-});
+router.put('/:id', controlle.put);
 
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-
-});
+router.delete('/', controlle.delete);
 
 module.exports = router;
 
